f427e77dd71e3b01d744ae4a4642ea764940320f
Jim Kriz <jim@kriz.net>
2023-12-15 08:02:46 -0500

    Day 15 - Lens Library

---
+++ src/test/kotlin/day15/LensLibraryTest.kt
@@ -0,0 +1,137 @@
+package day15
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import util.DataFiles
+
+@DisplayName("Day 15 - Lens Library")
+@TestMethodOrder(OrderAnnotation::class)
+class LensLibraryTest : DataFiles {
+    private val sampleSolver by lazy {
+        Solver(loadSampleInput())
+    }
+    private val solver by lazy {
+        Solver(loadInput())
+    }
+
+    @Test
+    fun `HASH result should be 52`() {
+        assertEquals(52, "HASH".computeHash())
+    }
+
+    @Test
+    @Order(1)
+    fun `Part 1 Sample Input should return 1320`() {
+        assertEquals(1320, sampleSolver.solvePartOne())
+    }
+
+    @Test
+    @Order(3)
+    fun `Part 2 Sample Input should return 145`() {
+        assertEquals(145, sampleSolver.solvePartTwo())
+    }
+
+    @Test
+    @Order(2)
+    fun `Part 1 Real Input should return 504449`() {
+        assertEquals(504449, solver.solvePartOne())
+    }
+
+    @Test
+    @Order(4)
+    fun `Part 2 Real Input should return 262044`() {
+        assertEquals(262044, solver.solvePartTwo())
+    }
+}
+
+class Solver(data: List<String>) {
+    private val lenses = data[0].split(",").map { Lens.fromInputString(it) }
+
+    fun solvePartOne(): Long =
+        lenses.sumOf {
+            it.inputValue.computeHash()
+        }
+
+    fun solvePartTwo(): Long {
+        val boxes = Boxes(lenses)
+        return boxes.focalLengths.sum()
+    }
+}
+
+data class Lens(val label: String, val operation: Char, val focalLength: Int) {
+    companion object {
+        fun fromInputString(inputString: String): Lens {
+            return if (inputString.contains('=')) {
+                val parts = inputString.split('=')
+                Lens(parts[0], '=', parts[1].toInt())
+            } else {
+                val parts = inputString.split('-')
+                Lens(parts[0], '-', 0)
+            }
+        }
+    }
+
+    val boxNumber: Int =
+        label.computeHash().toInt()
+
+    val isAddition: Boolean
+        get() = !isRemoval
+
+    val isRemoval: Boolean
+        get() = operation == '-'
+
+    val inputValue: String =
+        if (isAddition) {
+            "$label=$focalLength"
+        } else {
+            "$label-"
+        }
+}
+
+class Boxes(lenses: List<Lens>) {
+    val boxes: List<List<Lens>>
+
+    init {
+        val boxes = Array(256) { mutableListOf<Lens>() }
+        lenses.forEach { lens ->
+            val existingLens = boxes[lens.boxNumber].indexOfFirst { it.label == lens.label }
+            if (lens.isRemoval && existingLens >= 0) {
+                boxes[lens.boxNumber].removeAt(existingLens)
+            } else if (lens.isAddition) {
+                if (existingLens >= 0) {
+                    boxes[lens.boxNumber][existingLens] = lens
+                } else {
+                    boxes[lens.boxNumber].add(lens)
+                }
+            }
+        }
+
+        this.boxes = boxes.toList()
+    }
+
+    val focalLengths: List<Long>
+        get() = boxes.mapIndexed { boxNum, lenses ->
+            lenses.mapIndexed { slotNum, lens ->
+                ((1 + boxNum) * (1 + slotNum) * lens.focalLength).toLong()
+            }
+        }.flatten()
+}
+
+fun String.computeHash(): Long {
+    var result = 0L
+    this.forEach { char ->
+        result = result.addHash(char)
+    }
+    return result
+}
+
+fun Long.addHash(char: Char): Long =
+    if (char == '\n') {
+        this
+    } else {
+        ((this + char.code) * 17) % 256
+    }
